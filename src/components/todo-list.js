b4a15e6776b44245c4cf04d0965e7f266006ff70
Alena <9972211@gmail.com>
2021-09-20 20:32:47 +0300

    arrays as a property of components

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,11 +1,18 @@
 import TodoListItem from "./todo-list-item";
 
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
+
+    const elements = todos.map(item => {
+        return (
+            <li>
+                <TodoListItem {...item} />
+            </li>)
+    });
+
     return (
         <ul>
-            <li><TodoListItem /></li>
-            <li><TodoListItem /></li>
+            {elements}
         </ul>
     );
 };
