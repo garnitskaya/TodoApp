f2813fd70adf1f92011589d921b4497cd8f33456
Alena <9972211@gmail.com>
2021-09-21 16:08:29 +0300

    component class

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -1,16 +1,17 @@
+import { Component } from "react";
 import "./item-status-filter.css";
 
-const ItemStatusFilter = () => {
-    return (
-        <div className="btn-group">
-            <button type="button"
-                className="btn btn-info">All</button>
-            <button type="button"
-                className="btn btn-outline-secordary">Active</button>
-            <button type="button"
-                className="btn btn-outline-secordary">Done</button>
-        </div >
-    );
+export default class ItemStatusFilter extends Component {
+    render() {
+        return (
+            <div className="btn-group">
+                <button type="button"
+                    className="btn btn-info">All</button>
+                <button type="button"
+                    className="btn btn-outline-secordary">Active</button>
+                <button type="button"
+                    className="btn btn-outline-secordary">Done</button>
+            </div >
+        );
+    };
 };
-
-export default ItemStatusFilter;
